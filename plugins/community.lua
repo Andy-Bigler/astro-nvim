bb5b5112743f2f5be95115e3902373403c8262a9
Bigler Andy <andy.bigler@besonet.ch>
2023-10-04 22:45:30 +0200

    Initial setup

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,17 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.kotlin" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.json" },
 }
