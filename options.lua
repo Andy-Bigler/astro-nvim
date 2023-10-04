2fdc5a072f978a97b11f0a69a7e3478465ee3246
Bigler Andy <andy.bigler@besonet.ch>
2023-10-04 23:29:04 +0200

    Disable mouse and change cursor

--- options.lua
+++ options.lua
@@ -7,6 +7,8 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    mouse = "", -- disable mouse
+    guicursor = "n:hor20-Cursor/lCursor", -- underline cursor
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
